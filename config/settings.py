68224ba8297708dce807cc20fcaa4c203b4b5a97
Rodriii33xd <dispositivoshogartv@gmail.com>
2025-12-26 16:12:47 -0500

    Update and rename READ to settings.py

---
+++ config/settings.py
@@ -0,0 +1,6 @@
+# Idiomas OCR
+IDIOMAS = "spa+eng+fra+deu+ita+por"
+
+# Rutas del sistema (Windows)
+TESSERACT_PATH = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+POPPLER_PATH = r"C:\poppler\Library\bin"
