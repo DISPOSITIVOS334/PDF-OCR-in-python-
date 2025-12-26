a83d738af4a2e014f85845e335138f04fb874aa0
Rodriii33xd <dispositivoshogartv@gmail.com>
2025-12-26 16:13:53 -0500

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-# Idiomas OCR
-IDIOMAS = "spa+eng+fra+deu+ita+por"
-
-# Rutas del sistema (Windows)
-TESSERACT_PATH = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-POPPLER_PATH = r"C:\poppler\Library\bin"
